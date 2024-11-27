bda9a7ad2199e19414c9b272a42a3c5cdefe6a0b
TBJ <44341859+TBJr@users.noreply.github.com>
2024-11-27 16:14:38 -0500

    Add edit and delete functionality for recipes

--- app.py
+++ app.py
@@ -3,11 +3,26 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+def load_recipes():
+    with open('recipes.json') as f:
+        return json.load(f)
+
+def save_recipes(recipes):
+    with open('recipes.json', 'w') as f:
+        json.dump(recipes, f)
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
     with open('recipes.json') as f:
         recipes = json.load(f)
-    return render_template('/index.html', recipes=recipes)
+    filtered_recipes = [recipe for recipe in recipes if query.lower() in recipe['name'].lower()]
+    return render_template('index.html', recipes=filtered_recipes)
+
+@app.route('/')
+def index():
+    recipes = load_recipes()
+    return render_template('index.html', recipes=recipes)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_recipe():
@@ -24,13 +39,30 @@ def add_recipe():
         return redirect(url_for('index'))
     return render_template('add_recipe.html')
 
-@app.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query')
-    with open('recipes.json') as f:
-        recipes = json.load(f)
-    filtered_recipes = [recipe for recipe in recipes if query.lower() in recipe['name'].lower()]
-    return render_template('index.html', recipes=filtered_recipes)
+@app.route('/edit/<int:recipe_id>', methods=['GET', 'POST'])
+def edit_recipe(recipe_id):
+    recipes = load_recipes()
+    recipe = recipes[recipe_id]
+
+    if request.method == 'POST':
+        recipe['name'] = request.form['name']
+        recipe['description'] = request.form['description']
+        save_recipes(recipes)
+        return redirect(url_for('index'))
+
+    return render_template('edit_recipe.html', recipe=recipe, recipe_id=recipe_id)
+
+@app.route('/delete/<int:recipe_id>', methods=['GET', 'POST'])
+def delete_recipe(recipe_id):
+    recipes = load_recipes()
+
+    if request.method == 'POST':
+        recipes.pop(recipe_id)
+        save_recipes(recipes)
+        return redirect(url_for('index'))
+
+    recipe = recipes[recipe_id]
+    return render_template('delete_recipe.html', recipe=recipe)
 
 if __name__ == '__main__':
     app.run(debug=True)
