853353d6f25860c4b7867a740ee39f9365954322
TBJ <44341859+TBJr@users.noreply.github.com>
2024-11-27 15:25:11 -0500

    Major commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    with open('recipes.json') as f:
+        recipes = json.load(f)
+    return render_template('index.html', recipes=recipes)
 
 if __name__ == '__main__':
     app.run(debug=True)
